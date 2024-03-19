0fd5728f16f9d55dfb0d00760bba446138152089
Chad Yu <cky25@cornell.edu>
2024-03-19 04:40:32 -0400

    feature: ReplayBuffer for storing online transitions implemented

--- PA 3/buffer.py
+++ PA 3/buffer.py
@@ -13,19 +13,22 @@ class ReplayBuffer(object):
     def __init__(self, maxsize=100000):
         # TODO: Initialize the buffer using the given parameters.
         # HINT: Once the buffer is full, when adding new experience we should not care about very old data.
-        pass
+        self.buffer = deque(maxlen=maxsize)
 
     def __len__(self):
         # TODO: Return the length of the buffer (i.e. the number of transitions).
-        pass
+        return len(self.buffer)
 
     def add_experience(self, state, action, reward, next_state, done):
         # TODO: Add (s, a, r, s', d) to the buffer.
         # HINT: See the transition data type defined at the top of the file for use here.
-        pass
+        self.buffer.append(Transition(state, action, reward, next_state, done))
 
     def sample(self, batch_size):
         # TODO: Sample 'batch_size' transitions from the buffer.
         # Return a tuple of torch tensors representing the states, actions, rewards, next states, and terminal signals.
         # HINT: Make sure the done signals are floats when you return them.
-        pass
+        (
+            torch.Tensor(self.buffer[idx])
+            for idx in random.sample(range(len(self.buffer)), batch_size)
+        )
