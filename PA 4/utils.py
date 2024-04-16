d03be561502e15d53015e83130939b1351aaddc8
Chad Yu <cky25@cornell.edu>
2024-04-16 03:05:04 -0400

    feature: gradient of policy log-likelihood implemented

--- PA 4/utils.py
+++ PA 4/utils.py
@@ -6,7 +6,7 @@ rbf_feature = RBFSampler(gamma=1, random_state=12345)
 
 
 def extract_features(state, num_actions):
-    """ This function computes the RFF features for a state for all the discrete actions
+    """This function computes the RFF features for a state for all the discrete actions
 
     :param state: column vector of the state we want to compute phi(s,a) of (shape |S|x1)
     :param num_actions: number of discrete actions you want to compute the RFF features for
@@ -15,14 +15,14 @@ def extract_features(state, num_actions):
     s = state.reshape(1, -1)
     s = np.repeat(s, num_actions, 0)
     a = np.arange(0, num_actions).reshape(-1, 1)
-    sa = np.concatenate([s,a], -1)
+    sa = np.concatenate([s, a], -1)
     feats = rbf_feature.fit_transform(sa)
     feats = feats.T
     return feats
 
 
 def compute_softmax(logits, axis):
-    """ computes the softmax of the logits
+    """computes the softmax of the logits
 
     :param logits: the vector to compute the softmax over
     :param axis: the axis we are summing over
@@ -32,11 +32,13 @@ def compute_softmax(logits, axis):
     """
 
     # TODO
-    pass
+    return np.exp(logits - np.max(logits)) / np.sum(
+        np.exp(logits - np.max(logits)), axis=axis
+    )
 
 
 def compute_action_distribution(theta, phis):
-    """ compute probability distribution over actions
+    """compute probability distribution over actions
 
     :param theta: model parameter (shape d x 1)
     :param phis: RFF features of the state and actions (shape d x |A|)
@@ -44,11 +46,11 @@ def compute_action_distribution(theta, phis):
     """
 
     # TODO
-    pass
+    return compute_softmax(np.dot(theta.T, phis), axis=1)
 
 
 def compute_log_softmax_grad(theta, phis, action_idx):
-    """ computes the log softmax gradient for the action with index action_idx
+    """computes the log softmax gradient for the action with index action_idx
 
     :param theta: model parameter (shape d x 1)
     :param phis: RFF features of the state and actions (shape d x |A|)
@@ -57,14 +59,14 @@ def compute_log_softmax_grad(theta, phis, action_idx):
     """
 
     # TODO
-    pass
+    return phis[:, [action_idx]] - phis @ compute_action_distribution(theta, phis).T
 
 
 def compute_fisher_matrix(grads, lamb=1e-3):
-    """ computes the fisher information matrix using the sampled trajectories gradients
+    """computes the fisher information matrix using the sampled trajectories gradients
 
     :param grads: list of list of gradients, where each sublist represents a trajectory (each gradient has shape d x 1)
-    :param lamb: lambda value used for regularization 
+    :param lamb: lambda value used for regularization
 
     :return: fisher information matrix (shape d x d)
 
@@ -76,7 +78,7 @@ def compute_fisher_matrix(grads, lamb=1e-3):
 
 
 def compute_value_gradient(grads, rewards):
-    """ computes the value function gradient with respect to the sampled gradients and rewards
+    """computes the value function gradient with respect to the sampled gradients and rewards
 
     :param grads: ist of list of gradients, where each sublist represents a trajectory
     :param rewards: list of list of rewards, where each sublist represents a trajectory
@@ -87,9 +89,8 @@ def compute_value_gradient(grads, rewards):
     pass
 
 
-
 def compute_eta(delta, fisher, v_grad):
-    """ computes the learning rate for gradient descent
+    """computes the learning rate for gradient descent
 
     :param delta: trust region size
     :param fisher: fisher information matrix (shape d x d)
@@ -101,28 +102,50 @@ def compute_eta(delta, fisher, v_grad):
     pass
 
 
-
 def get_args():
-    parser = argparse.ArgumentParser(description='Imitation learning')
+    parser = argparse.ArgumentParser(description="Imitation learning")
 
     # general + env args
-    parser.add_argument('--data_dir', default='./data', help='dataset directory')
-    parser.add_argument('--env', default='CartPole-v0', help='environment')
-    
+    parser.add_argument("--data_dir", default="./data", help="dataset directory")
+    parser.add_argument("--env", default="CartPole-v0", help="environment")
+
     # learning args
-    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
-    parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-    parser.add_argument('--num_dataset_samples', type=int, default=10000, help='number of samples to start dataset off with')
-    
+    parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
+    parser.add_argument("--batch_size", type=int, default=64, help="batch size")
+    parser.add_argument(
+        "--num_dataset_samples",
+        type=int,
+        default=10000,
+        help="number of samples to start dataset off with",
+    )
+
     # DAGGER args
-    parser.add_argument('--dagger', action='store_true', help='flag to run DAGGER')
-    parser.add_argument('--expert_save_path', default='./learned_policies/NPG/expert_theta.npy')
-    parser.add_argument('--num_rollout_steps', type=int, help='number of steps to roll out with the policy')
-    parser.add_argument('--dagger_epochs', type=int, help='number of steps to run dagger')
-    parser.add_argument('--dagger_supervision_steps', type=int, help='number of epochs for supervised learning step within dagger')
-    
+    parser.add_argument("--dagger", action="store_true", help="flag to run DAGGER")
+    parser.add_argument(
+        "--expert_save_path", default="./learned_policies/NPG/expert_theta.npy"
+    )
+    parser.add_argument(
+        "--num_rollout_steps",
+        type=int,
+        help="number of steps to roll out with the policy",
+    )
+    parser.add_argument(
+        "--dagger_epochs", type=int, help="number of steps to run dagger"
+    )
+    parser.add_argument(
+        "--dagger_supervision_steps",
+        type=int,
+        help="number of epochs for supervised learning step within dagger",
+    )
+
     # model saving args
-    parser.add_argument('--policy_save_dir', default='./learned_policies', help='policy saving directory')
-    parser.add_argument('--state_to_remove', default=None, type=int, help='index of the state to remove')
-    
+    parser.add_argument(
+        "--policy_save_dir",
+        default="./learned_policies",
+        help="policy saving directory",
+    )
+    parser.add_argument(
+        "--state_to_remove", default=None, type=int, help="index of the state to remove"
+    )
+
     return parser.parse_args()
